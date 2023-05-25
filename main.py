0fbfd3ca11e666b5b5230c958b9aa30e4a71d268
shawn <mishawn331@gmail.com>
2023-05-25 08:48:23 +0600

    updated

--- main.py
+++ main.py
@@ -19,33 +19,88 @@ mysql=MySQL(app)
 def cal(ip):
     input = dict(ip)
    
-    day = input['day'][0]
-    weather = input['weather'][0]
-    light = input['light'][0]
-    Type_of_collision=input['Type_of_collision'][0]
-    Number_of_vehicles_involved=input['Number_of_vehicles_involved'][0]
-    Number_of_casualties=input['Number_of_casualties'][0]
-    Vehicle_movement=input['Vehicle_movement'][0]
+    day = input['day']
+    weather = input['weather']
+    light = input['light']
+    Type_of_collision=input['Type_of_collision']
+    Number_of_vehicles_involved=input['Number_of_vehicles_involved']
+    Number_of_casualties=input['Number_of_casualties']
+    Vehicle_movement=input['Vehicle_movement']
+    address=input['address']
 
     data = np.array([day,light,weather,Type_of_collision,Number_of_vehicles_involved,Number_of_casualties,Vehicle_movement])
-    print (data)
+    print (Number_of_casualties)
     
     data = data.reshape(1, -1)
+    print(data)
 
-    x = np.array([1, 3.73, 3, 0.69, 125, 4, 1, 1, 1, 1, 30]).reshape(1, -1)
+   
 
     try: result = model.predict(data)
     except Exception as e: result = str(e)
 
-    return str(result[0])
+    if day=='3' :
+         dayy="Sunday"
+    if day=='1' :
+         dayy="Monday"
+    if day=='5' :
+         dayy="Tuesday"
+    if day=='6' :
+         dayy="Wednesday"  
+    if day=='4' :
+         dayy="Thursday" 
+    if day=='0' :
+         dayy="Friday"
+    if day=='1' :
+         dayy="Saturday"
+
+    if weather=='2':
+         weatherr="Normal"
+    if weather=='4':
+         weatherr="Raining"     
+
+
+    if light=='3':
+         lightt="Daylight"
+    if light=='0':
+         lightt="Darkness-lights lit"  
+
+    if (int(Number_of_casualties)>20):
+         ans="Fatal"
+    elif (int(Number_of_casualties)>10 and int(Number_of_casualties)<20):
+         ans="Serious" 
+    else:
+         ans="Slight" 
+                      
+                      
+    cur=mysql.connection.cursor()
+    cur.execute ('insert into all_predicted_data(Day,Weather,Light,Type_of_collision,Number_of_vehicles_involve,Number_of_casualities,Address,Accident_severity ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)',(dayy,weatherr,lightt,Type_of_collision,int(Number_of_vehicles_involved),int(Number_of_casualties),address,ans))
+    mysql.connection.commit()
+    cur.close()                  
+     
+    
+    if(int(Number_of_casualties)>10 & int(Number_of_casualties)<20) :
+      return str(1);
+    elif(int(Number_of_casualties)>20) :
+         return str(0);
+    else:
+      return str(result[0])  
+    
+
+
+   
+    
 
 
 
 
 
 @app.route('/map/', methods=['GET'])
-def visual():
+def map():
     return render_template('map.html')
+@app.route('/logout',methods=['GET'])
+def logout():
+      return render_template('preindex.html')
 @app.route('/Home/',methods=['GET'])
 def home():
      return render_template('index.html')
@@ -71,7 +126,7 @@ def hello_world():
 def log():
      return render_template('login.html')
 
-@app.route('/form_login',methods=['POST','GET'])
+@app.route('/form_login',methods=['POST'])
 def login():
     name1=request.form['username']
     pwd=request.form['password']
@@ -80,8 +135,7 @@ def login():
     if user:
 	    return render_template('index.html')
     else:
-        
-            return render_template('login.html',info='Invalid Password/name')
+       return render_template('login.html',info='Invalid Password/name')
 @app.route('/sign',methods=['GET'])
 def sign():
      return render_template('sign.html')    
@@ -89,13 +143,19 @@ def sign():
 def signin():
     name1=request.form['username']
     pwd=request.form['password']
+    email=request.form['email']
     cur=mysql.connection.cursor()
-    cur.execute ('insert into register (Name,Password,Email) VALUES (%s,%s,%s)',(name1,pwd,'123'))
+    cur.execute ('insert into register (Name,Password,Email) VALUES (%s,%s,%s)',(name1,pwd,email))
     mysql.connection.commit()
     cur.close()
     return render_template('sign.html',info='succeed')
-     
-     
+
+@app.route('/visual/',methods=['GET'])
+def visual():
+     cur=mysql.connection.cursor()
+     cur.execute('select * from all_predicted_data')
+     alldata = cur.fetchall()
+     return render_template('visual.html',alldata=alldata)          
 @app.route('/', methods=['POST'])
 
 def get():
